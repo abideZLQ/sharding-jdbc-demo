ddfc1dc518def82440ac079008b4b68efd659b5e
zlqwork <1756550688@qq.com>
2021-07-09 17:59:41 +0800

    demo commit

---
+++ pmpp/pmpp-api/src/main/java/com/ctvit/PmppApplication.java
@@ -0,0 +1,15 @@
+package com.ctvit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class PmppApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run(PmppApplication.class, args);
+    }
+}
